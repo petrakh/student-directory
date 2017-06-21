f48685f5a987d4efe81e220dfcd5624186af58bb
petrakh <petra.chocholova@gmail.com>
2017-06-21 22:30:39 +0200

    Only students beginning with P will show - Excercise 2

--- directory.rb
+++ directory.rb
@@ -23,8 +23,10 @@ def print_header
 end
 
 def print(students)
-  students.each_with_index do |student, i|
-    puts "#{i+1}. #{student[:name]} (#{student[:cohort]} cohort)"
+  students.each do |student|
+    if student[:name].chars.first == "P"
+      puts "#{student[:name]} (#{student[:cohort]} cohort)"
+    end
   end
 end
 
@@ -32,21 +34,8 @@ def print_footer(students)
   puts "Overall, we have #{students.count} great students"
 end
 students = input_students
+
 #nothing happens until we call the methods
 print_header
 print(students)
 print_footer(students)
-
-#students = [
-#  {name: "Dr. Hannibal Lecter", cohort: :november},
-#  {name: "Darth Vader", cohort: :november},
-#  {name: "Nurse Ratched", cohort: :november},
-#  {name: "Michael Corleone", cohort: :november},
-#  {name: "Alex DeLarge", cohort: :november},
-#  {name: "The Wicked Witch of the West", cohort: :november},
-#  {name: "Terminator", cohort: :november},
-#  {name: "Freddy Krueger", cohort: :november},
-#  {name: "The Joker", cohort: :november},
-#  {name: "Joffrey Baratheon", cohort: :november},
-#  {name: "Norman Bates", cohort: :november}
-#]
