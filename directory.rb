1c92addf500497bf0ac2dd031b7a440873f9a7fd
petrakh <petra.chocholova@gmail.com>
2017-06-23 21:01:22 +0200

    8.8. Printing students by their cohorts

--- directory.rb
+++ directory.rb
@@ -14,12 +14,13 @@ def input_students
     hobby = gets.chomp
     puts "What is their country of birth?"
     country = gets.chomp
-    students << {name: name, cohort: cohort, hobby: hobby, country: country}
+    students << {name: name, cohort: cohort.to_sym, hobby: hobby, country: country}
     puts "Now we have #{students.count} students"
     # get another name from the user
     puts "Enter a new student name or to finish, just hit return twice"
     name = gets.chomp
   end
+  students = students.sort_by {|student| cohort_list.index(student[:cohort])}
   students
 end
 
@@ -28,11 +29,12 @@ def print_header
   puts "-------------".center(100)
 end
 
+
+
+
 def print(students)
-  n = 0
-  until n == students.count
-  puts "#{students[n][:name]} (#{students[n][:cohort]} cohort, hobby: #{students[n][:hobby]}, country of origin: #{students[n][:country]})".center(100)
-  n += 1
+  students.each do |student|
+  puts "#{student[:name]} (#{student[:cohort]} cohort, hobby: #{student[:hobby]}, country of origin: #{student[:country]})".center(100)
   end
 end
 
