a24826dbfbc75e2043a7e9ddd42fff1b26fdd8cb
petrakh <petra.chocholova@gmail.com>
2017-06-23 22:33:50 +0200

    10. Interactive menu refactored, code simplified overall

--- directory.rb
+++ directory.rb
@@ -1,49 +1,55 @@
+@students = [] # an empty array accessible to all methods
+
 def interactive_menu
-  students = []
   loop do
-    # 1. print the menu and ask the user what to do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit"
-    # 2. read the input and same it into a variable
-    selection = gets.chomp
-    # 3. do what user has asked
-    case selection
-      when "1"
-        students = input_students
-      when "2"
-        print_header
-        print(students)
-        print_footer(students)
-      when "9"
-        exit
-      else
-        puts "I don't know what you meant, try again please"
-    end
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end
+
+def process(selection)
+  case selection
+    when "1"
+      @students = input_students
+    when "2"
+      show_students
+    when "9"
+      exit
+    else
+      puts "I don't know what you meant, try again please"
   end
 end
 
 def input_students
   puts "Please enter the names of the students"
-  students = []
-  name = gets
-  name.gsub!("\n","")
+  name = gets.chomp
   while !name.empty? do
     puts "What is their cohort?"
     cohort_list = [:january, :february, :march, :april, :may, :june, :july, :august, :september, :october, :november, :december]
     cohort = gets.chomp
-      unless cohort_list.include? cohort.to_sym
+      until cohort_list.include? cohort.to_sym
         puts "This is not a valid month, please enter a valid one"
         cohort = gets.chomp
       end
-    students << {name: name, cohort: cohort.to_sym}
-    puts "Now we have #{students.count} students"
+    @students << {name: name, cohort: cohort.to_sym}
+    puts "Now we have #{@students.count} students"
     puts "Enter a new student name or to finish, just hit return twice"
-    name = gets
-    name.gsub!("\n","")
+    name = gets.chomp
   end
-  students = students.sort_by {|student| cohort_list.index(student[:cohort])}
-  students
+  @students = @students.sort_by {|student| cohort_list.index(student[:cohort])}
+  @students
 end
 
 def print_header
@@ -51,9 +57,9 @@ def print_header
   puts "-------------".center(100)
 end
 
-def print(students)
-  if students.count >= 1
-    students.each do |student|
+def print_students_list
+  if @students.count >= 1
+    @students.each do |student|
       puts "#{student[:name]} (#{student[:cohort]} cohort)".center(100)
     end
   else
@@ -61,18 +67,13 @@ def print(students)
   end
 end
 
-def print_footer(students)
-  if students.count == 0
+def print_footer
+  if @students.count == 0
     puts "We have zero students".center(100)
-  elsif students.count < 2
-    puts "Overall, we have #{students.count} great student".center(100)
+  elsif @students.count < 2
+    puts "Overall, we have #{@students.count} great student".center(100)
   else
-    puts "Overall, we have #{students.count} great students".center(100)
+    puts "Overall, we have #{@students.count} great students".center(100)
   end
 end
-students = interactive_menu
-
-
-print_header
-print(students)
-print_footer(students)
+@students = interactive_menu
