6a9ddae9d3d77e5be07d83df31e3d81ed288e661
petrakh <petra.chocholova@gmail.com>
2017-06-23 19:56:16 +0200

    8.6. The output is now beautifully aligned using centre()

--- directory.rb
+++ directory.rb
@@ -17,20 +17,20 @@ def input_students
 end
 
 def print_header
-  puts "The students of Villains Academy"
-  puts "-------------"
+  puts "The students of Villains Academy".center(100)
+  puts "-------------".center(100)
 end
 
 def print(students)
   n = 0
   until n == students.count
-  puts "#{students[n][:name]} (#{students[n][:cohort]} cohort, hobby: #{students[n][:hobby]}, country of origin: #{students[n][:country]})"
+  puts "#{students[n][:name]} (#{students[n][:cohort]} cohort, hobby: #{students[n][:hobby]}, country of origin: #{students[n][:country]})".center(100)
   n += 1
   end
 end
 
 def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
+  puts "Overall, we have #{students.count} great students".center(100)
 end
 students = input_students
 
